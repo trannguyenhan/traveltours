5fa0d228f566cae4db186e80c5c339d1f662230d
trannguyenhan <trannguyenhan01092000@gmail.com>
2022-07-21 22:51:57 +0700

    add filter

--- traveltours-frontend/src/components/Tours/Filter/Filter.vue
+++ traveltours-frontend/src/components/Tours/Filter/Filter.vue
@@ -1,45 +1,50 @@
 <template>
   <div>
+    <v-row align="center">
+      <H3TSearch />
+    </v-row>
     <v-row align="center" justify="center">
       <v-col cols="6">
-        <TrnDestinations />
+        <H3TDestinations />
       </v-col>
 
       <v-col cols="6">
-        <TrnTravelStyle />
+        <H3TTravelStyle />
       </v-col>
     </v-row>
 
     <v-row class="mt-1" justify="center">
       <v-col cols="6" align="center">
-        <TrnPrice />
+        <H3TPrice />
       </v-col>
 
       <v-col cols="3">
-        <TrnRating />
+        <H3TRating />
       </v-col>
 
       <v-col cols="3">
-        <TrnDuration />
+        <H3TDuration />
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-  import TrnDestinations from './Destinations';
-  import TrnTravelStyle from './TravelStyle';
-  import TrnPrice from './Price';
-  import TrnRating from './Rating';
-  import TrnDuration from './Duration';
+  import H3TSearch from './Search';
+  import H3TDestinations from './Destinations';
+  import H3TTravelStyle from './TravelStyle';
+  import H3TPrice from './Price';
+  import H3TRating from './Rating';
+  import H3TDuration from './Duration';
 
   export default {
     components: {
-      TrnDestinations,
-      TrnTravelStyle,
-      TrnPrice,
-      TrnRating,
-      TrnDuration,
+      H3TSearch,
+      H3TDestinations,
+      H3TTravelStyle,
+      H3TPrice,
+      H3TRating,
+      H3TDuration,
     },
   };
 </script>
---
+++ traveltours-frontend/src/components/Tours/Filter/Search.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-text-field
+    v-model="keyword"
+    class="search-bar"
+    type="text"
+    label="Tìm kiếm tour..."
+  />
+</template>
+
+<script>
+  import syncSelectionWithUrl from './mixins/syncSelectionWithUrl';
+  import { defaultSelection } from './mixins/selection';
+
+  export default {
+    mixins: [
+      syncSelectionWithUrl('keyword'),
+      defaultSelection('keyword'), // need to put in last to override method hasSelectionValue()
+    ],
+  };
+</script>
+
+<style scoped>
+  .search-bar {
+    width: 100%;
+    display: block;
+    padding-top: 20px;
+    padding-left: 45px;
+    background: url('../../../assets/search_icon.png') no-repeat 15px center;
+    background-size: 15px 15px;
+    font-size: 16px;
+    border: none;
+    border-radius: 5px;
+  }
+</style>
