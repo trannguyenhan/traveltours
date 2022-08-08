bb47496d77635aea4be439ed1a70f98423393c96
duongntt <nttduong09091999@gmail.com>
2022-08-08 20:15:24 +0700

    ok

--- traveltours-backend/database/seeders/UserSeeder.php
+++ traveltours-backend/database/seeders/UserSeeder.php
@@ -34,6 +34,12 @@ class UserSeeder extends Seeder
         ]);
 
         DB::table('model_has_roles')->insert([
+            'role_id' => 1,
+            'model_id' => 1,
+            'model_type' => \App\Models\User::class
+        ]);
+
+        DB::table('model_has_roles')->insert([
             'role_id' => 3,
             'model_id' => 2,
             'model_type' => \App\Models\User::class
